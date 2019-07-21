cff8ff1e6702571f883731fc2ae00b67ffbe33d5
Anubhav Goel <replyag96@gmail.com>
2019-07-21 12:57:52 +0530

    mongoose dbConnection and routes setup

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
+const usersRoutes = require('./routes/api/users');
+const authRoutes = require('./routes/api/auth');
+const profileRoutes = require('./routes/api/profile');
+const postsRoutes = require('./routes/api/posts');
 const app = express();
-const PORT = process.env.PORT || 5000;
+//Connect to database
+connectDB();
 
 app.get('/', (req, res) => {
-  res.send('API Running!');
+    res.send('API Running!');
 });
 
+//Define routes
+app.use('/api/users', usersRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/profile', profileRoutes);
+app.use('/api/posts', postsRoutes);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}..`);
+    console.log(`Server started on port ${PORT}..`);
 });
