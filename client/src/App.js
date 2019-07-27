bbf2ccafbda4b16c62bf713bffc3c7ced1704859
Anubhav Goel <replyag96@gmail.com>
2019-07-27 20:21:48 +0530

    Redux setup and alerts

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 import React, { Fragment } from 'react';
 import './App.css';
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import Alert from './components/layout/Alert';
 
 const App = () => {
     return (
-        <Fragment>
-            <h1>Hello World!</h1>
-        </Fragment>
+        <Provider store={store}>
+            <Router>
+                <Fragment>
+                    <Navbar />
+                    <Route exact path='/' component={Landing} />
+                    <section className='container'>
+                        <Alert />
+                        <Switch>
+                            <Route
+                                exact
+                                path='/register'
+                                component={Register}
+                            />
+                            <Route exact path='/login' component={Login} />
+                        </Switch>
+                    </section>
+                </Fragment>
+            </Router>
+        </Provider>
     );
 };
 
